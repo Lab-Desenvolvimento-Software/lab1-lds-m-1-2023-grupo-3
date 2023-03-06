ff797d72fd77b4473120360d472bba3c3247e80a
André Mendes <afmendes@sga.pucminas.br>
2023-03-05 22:53:16 -0300

    Update Usuario.java

--- implementacao/Usuario.java
+++ implementacao/Usuario.java
@@ -2,11 +2,11 @@ public class Usuario {
     private String nome;
     private int cpf;
     private String endereco;
-    private String telefone;
+    private int telefone;
     private String email;
     private String senha;
 
-    public void cadastrarUsuario(String nome, int cpf, String endereco, String telefone, String email, String senha){
+    public void cadastrarUsuario(String nome, int cpf, String endereco, int telefone, String email, String senha){
         this.nome - nome;
         this.cpf = cpf;
         this.endereco = endereco;
