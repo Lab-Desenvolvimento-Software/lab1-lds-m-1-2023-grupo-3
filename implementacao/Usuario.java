5bb05524453f6a70f89ac127f6b537be24bcab3d
André Mendes <afmendes@sga.pucminas.br>
2023-03-05 21:02:13 -0300

    Update Usuario.java

--- implementacao/Usuario.java
+++ implementacao/Usuario.java
@@ -6,7 +6,11 @@ public class Usuario {
     private String endereco;
     private String telefone;
 
+    public void cadastrarUsuario(String nome, int cpf, String endereco, String telefone, String email, String senha){
+    
 
+    }
+    
     public void logar(String email, String senha){
 
     }
