cb5ef2cc19e0d460890dd5962f4385ab256ec525
Andre Mendes <afmen@sga.pucminas.br>
2023-03-13 23:01:44 -0300

    Atualização

--- implementacao/Aplicacao.java
+++ implementacao/Aplicacao.java
@@ -11,9 +11,9 @@ public class Aplicacao {
         Disciplina matematica = new Disciplina(1, "matemática", false);
         Disciplina portugues = new Disciplina(2, "português", false);
         Disciplina historia = new Disciplina(3, "história", true);
-        oCurso.adicionarDisciplina(matematica);
-        oCurso.adicionarDisciplina(portugues);
-        oCurso.adicionarDisciplina(historia);
+        //oCurso.adicionarDisciplina(matematica);
+        //oCurso.adicionarDisciplina(portugues);
+        //oCurso.adicionarDisciplina(historia);
         Professor prof1 = new Professor(100, "Américo", 123321, "rua #1", 12321, 'p', "email.com", "123");
         Professor prof2 = new Professor(100, "Bernardo", 321456, "rua #2", 456878, 'p', "email.com.br", "14523");
         Aluno aluno1 = new Aluno(100, "Alice", 123321, "rua #1", 133546, 'a', "email.com", "123");
@@ -58,45 +58,22 @@ public class Aplicacao {
 
                 case 5:
                     System.out.println("Opção 05: Consultar cursos");
-
+                    Curso.consultarCurso();
+                    break;
 
                 case 6:
-                    System.out.println("06. Adicionar disciplina em um curso");
-                    System.out.println("Decida em qual curso adicionar a disciplina: ");
-                    for (int i = 0; i < Curso.cursos.size(); i++) {
-                        System.out.println(i + " nome: " + Curso.cursos.get(i).nome);
-                    }
-                    temp = info.nextInt();
-                    cur = Curso.cursos.get(temp);
-                    System.out.println("Escolha a disciplina: ");
-                    for (int i = 0; i < Disciplina.disciplinas.size(); i++) {
-                        System.out.println(i + " nome: " + Disciplina.disciplinas.get(i));
-                    }
-                    temp = info.nextInt();
-                    cur.adicionarDisciplina(Disciplina.disciplinas.get(temp));
+                    System.out.println("Opção 06: Adicionar disciplina em um curso");
+                    Curso.adicionarDisciplina();
                     break;
 
                 case 7:
                     System.out.println("07. Excluir disciplina de um curso");
-                    System.out.println("Decida em qual curso excluir a disciplina: ");
-                    for (int i = 0; i < Curso.cursos.size(); i++) {
-                        System.out.println(i + " nome: " + Curso.cursos.get(i).nome);
-                    }
-                    temp = info.nextInt();
-                    cur = Curso.cursos.get(temp);
-                    System.out.println("Escolha a disciplina: ");
-                    for (int i = 0; i < Disciplina.disciplinas.size(); i++) {
-                        System.out.println(i + " nome: " + Disciplina.disciplinas.get(i));
-                    }
-                    temp = info.nextInt();
-                    Disciplina.disciplinas.remove(temp);
+                    Curso.excluirDisciplina();
                     break;
 
                 case 8:
                     System.out.println("08. Consultar disciplinas de um curso");
-                    for (int i = 0; i < Curso.cursos.size(); i++) {
-                        System.out.println(i + " nome: " + Curso.cursos.get(i).nome);
-                    }
+                    Curso.consultarDisciplina();
                     break;
 
                 case 9:
--- implementacao/Curso.java
+++ implementacao/Curso.java
@@ -1,34 +1,33 @@
 import java.util.LinkedList;
 import java.util.Scanner;
 
-
 public class Curso {
 
     private int codigoCurso;
     private int creditos;
     private static int temp;
-    public String nome;
-    public LinkedList<Disciplina> disciplinas= new LinkedList<Disciplina>();
+    public static String nomeCurso;
+    public static Curso cur;
+    public static LinkedList<Disciplina> disciplinas = new LinkedList<Disciplina>();
     public static LinkedList<Curso> cursos = new LinkedList<Curso>();
 
-    Curso (int codigoCurso, int creditos, String nome){
+    Curso(int codigoCurso, int creditos, String nomeCurso) {
         this.codigoCurso = codigoCurso;
         this.creditos = creditos;
-        this.nome = nome;
+        this.nomeCurso = nomeCurso;
         cursos.add(this);
     }
-    
+
     public LinkedList<Disciplina> consultarDisciplinas() {
         return disciplinas;
     }
 
     public LinkedList<Disciplina> consultarProfessor(Professor prof) {
-        //retorna uma lista de disciplinas que o professor leciona
+        // retorna uma lista de disciplinas que o professor leciona
         return null;
     }
 
-    public static void adicionarCurso(){
-        String nomeCurso;
+    public static void adicionarCurso() {
         int creditos, codigo;
         Scanner info = new Scanner(System.in);
         System.out.println("informe o nome do curso: ");
@@ -40,23 +39,56 @@ public class Curso {
 
     }
 
-    public static void excluirCurso(){
+    public static void excluirCurso() {
         Scanner info = new Scanner(System.in);
-        for (int i=0; i<cursos.size(); i++){
-            System.out.println(i + " nome: " + cursos.get(i).nome);
-        }
-        temp = info.nextInt();
-        cursos.remove(temp);  
-    }
-
-    public static void consultarCurso(){
         for (int i = 0; i < cursos.size(); i++) {
             System.out.println(i + " nome: " + cursos.get(i).nomeCurso);
         }
-        break;
+        temp = info.nextInt();
+        cursos.remove(temp);
     }
 
-    public void adicionarDisciplina(Disciplina disciplina) {
-        disciplinas.add(disciplina);
+    public static void consultarCurso() {
+        for (int i = 0; i < cursos.size(); i++) {
+            System.out.println(i + " nome: " + cursos.get(i).nomeCurso);
+        }
+    }
+
+    public static void adicionarDisciplina() {
+        Scanner info = new Scanner(System.in);
+        System.out.println("Decida em qual curso adicionar a disciplina: ");
+        for (int i = 0; i < cursos.size(); i++) {
+            System.out.println(i + " nome: " + cursos.get(i).nomeCurso);
+        }
+        temp = info.nextInt();
+        cur = cursos.get(temp);
+        System.out.println("Escolha a disciplina: ");
+        for (int i = 0; i < Disciplina.disciplinas.size(); i++) {
+            System.out.println(i + " nome: " + Disciplina.disciplinas.get(i));
+        }
+        temp = info.nextInt();
+        disciplinas.add(Disciplina.disciplinas.get(temp));
+    }
+
+    public static void excluirDisciplina() {
+        Scanner info = new Scanner(System.in);
+        System.out.println("Decida em qual curso excluir a disciplina: ");
+        for (int i = 0; i < cursos.size(); i++) {
+            System.out.println(i + " nome: " + cursos.get(i).nomeCurso);
+        }
+        temp = info.nextInt();
+        cur = Curso.cursos.get(temp);
+        System.out.println("Escolha a disciplina: ");
+        for (int i = 0; i < Disciplina.disciplinas.size(); i++) {
+            System.out.println(i + " nome: " + Disciplina.disciplinas.get(i));
+        }
+        temp = info.nextInt();
+        Disciplina.disciplinas.remove(temp);
+    }
+
+    public static void consultarDisciplina() {
+        for (int i = 0; i < cursos.size(); i++) {
+            System.out.println(i + " nome: " + cursos.get(i).nomeCurso);
+        }
     }
 }
